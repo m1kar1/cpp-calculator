1369743a2eeab116af4ea2aff535bbf55a66c1f0
m1kar1 <an.selenkov@yandex.ru>
2026-02-03 20:01:19 +0300

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -1,6 +1,35 @@
 #pragma once
 
+#include <string>
+
 using Number = double;
 
-bool ReadNumber(Number& result);
-bool RunCalculatorCycle();
+class Calculator {
+public:
+    Calculator(Number init = 0) : number_(init) {
+    }
+    void Add(Number r);
+    void Sub(Number r);
+    void Div(Number r);
+    void Mul(Number r);
+    void Pow(Number r);
+
+    void Set(Number n);
+    void Negate(Number n);
+
+    void Save();
+    void Load();
+
+    void ClearMemory();
+
+    bool GetHasMem() const;
+
+    Number GetNumber() const;
+
+    std::string GetNumberRepr() const;
+
+private:
+    bool has_mem_ = false;
+    Number number_;
+    Number mem_;
+};
